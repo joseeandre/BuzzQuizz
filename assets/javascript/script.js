bd742d681f54943f5b89405a2be570dea767f874
barbielm <barbosagabrie0508l@gmail.com>
2021-04-24 20:27:31 -0300

    mostrar resultado e reiniciar quizz

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,11 @@
 let quizzlist
 let selectedQuizz
-const isQuestionInBlank = []
+let isQuestionInBlank = []
+let numberOfCorrectQuestions = 0
+
 let comparator = () => Math.random() - 0.5
 let getLastElement = (list) => list[list.length - 1]
+
 getQuizzList()
 
 
@@ -25,6 +28,7 @@ function quizzesError(){
 
 function showQuizz(quizzId){
     const quizz = document.querySelector(".selected-quizz")
+    quizz.scrollIntoView({behavior: "smooth"})
     quizz.classList.remove("hidden")
     const questions = quizz.querySelector(".questions")
     for(let i = 0; i < quizzlist.length; i++){
@@ -41,7 +45,13 @@ function showQuizz(quizzId){
         let question = selectedQuizz.questions[i]
         questions.innerHTML += writeQuestion(i,question)
     }
+    questions.innerHTML += `<div class="level hidden"></div>`
     quizz.querySelector(".question").classList.remove("hidden")
+    setTimeout(scrollToFirstQuestion,2000,quizz)
+}
+
+function scrollToFirstQuestion(quizz){
+    quizz.querySelector(".question").scrollIntoView({behavior: "smooth"})
 }
 
 function writeQuestion(id,question){
@@ -85,14 +95,85 @@ function selectOption(option){
             }
         }
         option.classList.remove("blurry")
-
+        if(option.classList.contains("correct-answer")){
+            numberOfCorrectQuestions++
+        }
+        checkIfLastQuestion(question)
+        isQuestionInBlank[id] = false
+        setTimeout(scrollToNextQuestion, 2000, question)
     }
-    isQuestionInBlank[id] = false
-    setTimeout(scrollToNextQuestion, 2000, question)
+}
+
+
+function checkIfLastQuestion(question){
+    if(question === getLastElement(document.querySelector(".selected-quizz").querySelectorAll(".question"))){
+        getLevel()
+    }
+}
+
+function getLevel(){
+    const rate = Math.round(100*numberOfCorrectQuestions/(selectedQuizz.questions.length))
+    const levels = selectedQuizz.levels
+    let level
+    const minValue = binarySearch(levels,rate)
+    for(let i = 0; i < levels.length; i++){
+        if(levels[i].minValue === minValue){
+            level = levels[i]
+        }
+    }
+    writeLevel(rate,level.title,level.image,level.text)
+}
+
+function writeLevel(rate,title,image,text){
+    document.querySelector(".selected-quizz .level").innerHTML += ` <div class="title">${rate}% de acerto: ${title}</div>
+                                                    <div class="description">
+                                                        <img src="${image}" />
+                                                        <span><strong>${text}</strong></span>
+                                                    </div>`
+    showButtons()
+}
+
+function showButtons(){
+    const quizz = document.querySelector(".selected-quizz")
+    quizz.querySelectorAll("button")[0].classList.remove("hidden")
+    quizz.querySelectorAll("button")[1].classList.remove("hidden")
+}
+
+function hideButtons(){
+    const quizz = document.querySelector(".selected-quizz")
+    quizz.querySelectorAll("button")[0].classList.add("hidden")
+    quizz.querySelectorAll("button")[1].classList.add("hidden")
 }
 
 function scrollToNextQuestion(question){
     const nextQuestion = question.nextElementSibling
     nextQuestion.classList.remove("hidden")
-    nextQuestion.scrollIntoView()
+    nextQuestion.scrollIntoView({behavior: "smooth"})
+}
+
+function binarySearch(list,key){
+    if(list.length === 1){
+        return list[0].minValue
+    } else {
+        let middle = Math.floor(list.length/2)
+        const halfList = []
+        if(key < list[middle].minValue){
+            for(let i = 0; i < middle; i++){
+                halfList.push(list[i])
+            }
+        } else {
+            for(let i = middle; i < list.length; i++){
+                halfList.push(list[i])
+            }
+        }
+        return binarySearch(halfList,key)
+    }
+}
+
+function remakeQuizz(){
+    numberOfCorrectQuestions = 0
+    isQuestionInBlank = []
+    setAllQuestionsInBlank(selectedQuizz)
+    hideButtons()
+    showQuizz(selectedQuizz.id)
 }
