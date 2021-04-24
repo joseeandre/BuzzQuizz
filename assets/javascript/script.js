803fbd8d1ecc1dcbb5f0e24547a92788d3764d31
barbielm <barbosagabrie0508l@gmail.com>
2021-04-24 12:28:55 -0300

    scroll para a próxima pergunta

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,11 @@
 let quizzlist
-const isQuestionInBlank = [true,true]
+let selectedQuizz
+const isQuestionInBlank = []
+let comparator = () => Math.random() - 0.5
+let getLastElement = (list) => list[list.length - 1]
 getQuizzList()
+
+
 function getQuizzList(){
     const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes")
     promise.then(loadQuizzes)
@@ -12,50 +17,52 @@ function loadQuizzes(reply){
     
 }
 
-function writeQuizz(quizzId){
-    let selectedQuizz
-    const quizz = document.querySelector(".quizz")
+function quizzesError(){
+    alert("Não foi possível obter os quizzes !")
+    location.reload()
+}
+
+
+function showQuizz(quizzId){
+    const quizz = document.querySelector(".selected-quizz")
     quizz.classList.remove("hidden")
     const questions = quizz.querySelector(".questions")
-    questions.innerHTML = ""
     for(let i = 0; i < quizzlist.length; i++){
         if(parseInt(quizzId) === quizzlist[i].id){
             selectedQuizz = quizzlist[i]
         }
     }
+    setAllQuestionsInBlank(selectedQuizz)
+    questions.innerHTML = ""
+    quizz.querySelector(".quizz-image").innerText = selectedQuizz.title
     quizz.querySelector(".quizz-image").style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${selectedQuizz.image})`
     for(let i = 0; i < selectedQuizz.questions.length; i++){
-        questions.innerHTML += `<div class="question" id="${i}">
-                                    <div class="title" style="background-color:${selectedQuizz.questions[i].color}">${selectedQuizz.questions[i].title}</div>
-                                    <div class="option" onclick="selectOption(this)">
-                                        <img src=${selectedQuizz.questions[i].answers[0].image} />
-                                        <div class="answer">${selectedQuizz.questions[i].answers[0].text}</div>
-                                    </div>
-                                    <div class="option" onclick="selectOption(this)">
-                                        <img src=${selectedQuizz.questions[i].answers[1].image} />
-                                        <div class="answer">${selectedQuizz.questions[i].answers[1].text}</div>
-                                    </div>
-                                    <div class="option" onclick="selectOption(this)">
-                                        <img src=${selectedQuizz.questions[i].answers[2].image} />
-                                        <div class="answer">${selectedQuizz.questions[i].answers[2].text}</div>
-                                    </div>
-                                    <div class="option" onclick="selectOption(this)">
-                                        <img src=${selectedQuizz.questions[i].answers[3].image} />
-                                        <div class="answer">${selectedQuizz.questions[i].answers[3].text}</div>
-                                    </div>
-                                </div>`
-    
+        selectedQuizz.questions[i].answers.sort(comparator)
+        let question = selectedQuizz.questions[i]
+        questions.innerHTML += writeQuestion(i,question)
     }
+    quizz.querySelector(".question").classList.remove("hidden")
 }
 
-function generateQuestion(){
+function writeQuestion(id,question){
 
+    let divQuestion = `<div class="question hidden" id="${id}">
+                            <div class="title" style="background-color:${question.color}">${question.title}</div>`
+    for(i = 0; i < question.answers.length; i++){
+        let answer = question.answers[i]
+        divQuestion += writeAnswer(answer)
+    }
+    divQuestion += `</div>`
+    return divQuestion
 }
 
-function quizzesError(){
-    alert("Não foi possível obter os quizzes !")
-    location.reload()
+function writeAnswer(answer){
+    return `<div class="option" onclick="selectOption(this)">
+                <img src=${answer.image} />
+                <div class="answer">${answer.text}</div>
+            </div>`
 }
+
 
 function setAllQuestionsInBlank(quizz){
     for(let i = 0; i < quizz.questions.length; i++){
@@ -71,8 +78,21 @@ function selectOption(option){
         const options = question.querySelectorAll(".option")
         for(let i = 0; i < options.length; i++){
             options[i].classList.add("blurry")
+            if(selectedQuizz.questions[id].answers[i].isCorrectAnswer){
+                options[i].classList.add("correct-answer")    
+            } else{
+                options[i].classList.add("wrong-answer")
+            }
         }
         option.classList.remove("blurry")
+
     }
     isQuestionInBlank[id] = false
+    setTimeout(scrollToNextQuestion, 2000, question)
+}
+
+function scrollToNextQuestion(question){
+    const nextQuestion = question.nextElementSibling
+    nextQuestion.classList.remove("hidden")
+    nextQuestion.scrollIntoView()
 }
