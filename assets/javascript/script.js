4994f87f6ced131ca14825d609bf5a3c09862dbd
Jose Andre <jose.andrenunes5@gmail.com>
2021-04-23 21:27:13 -0300

    seus_commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,19 @@
 let quizzes = [];
-let quizzesHTML = document.querySelector(".quizzes");
+let quizzesHTML = document.querySelector(".todos-quizzes").querySelector(".quizzes");
+let meusquizzesHTML = document.querySelector(".meus-quizzes").querySelector(".quizzes");
 
 function aoEntrarNaPagina() {
+    meusQuizzes();
     carregarQuizzes();
+}
+
+function meusQuizzes() {
+    let criarQuiz = document.querySelector(".criar-quiz");
+    let meuQuiz = document.querySelector(".meus-quizzes");
+    if (localStorage != null) {
+        criarQuiz.style.display = "none";
+        meuQuiz.classList.remove("oculto");
+    }
 }
 
 function carregarQuizzes() {
@@ -21,10 +32,16 @@ function processarQuizzes(resposta) {
     for (let i = 0; i < quizzes.length; i++) {
         foto = quizzes[i].image;
         titulo = quizzes[i].title;
-        novoQuizz = '<div class="quiz"><img src=' + foto + ' alt=""><div class="legenda-quiz">' + titulo + '</div></div>'
+        id = quizzes[i].id;
+        novoQuizz = '<div class="quiz"><img src=' + foto + ' alt=""><div class="legenda-quiz">' + titulo + '</div><div class="gradiente"></div></div>'
 
-        quizzesHTML.innerHTML += novoQuizz;
+        if (localStorage.getItem(id.toString()) != null) {
+            meusquizzesHTML.innerHTML += novoQuizz;
+        } else {
+            quizzesHTML.innerHTML += novoQuizz;
+        }
     }
 }
+
 
 aoEntrarNaPagina();
